ff56a646b725dfd20c4d3ddb2cd45846e4569a30
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2018-11-26 23:08:14 -0600

    Initial Version 1.0.0

---
+++ example.py
@@ -0,0 +1,47 @@
+#=================== ABOUT ===================#
+# table_printer_py/example.py
+# A python table printer for a given dataset
+# Author: Martín Figueroa Padilla
+# Version = 1.0.0
+#=============================================#
+
+from printer import TablePrinter
+
+table = TablePrinter()
+table.add_row(['Hermione Granger', 'Harry Potter'])
+table.add_row(['Simon Spier', 'Simon vs. the Homo Sapiens Agenda'])
+table.add_row(['Ally', 'A Star is Born'])
+table.add_row(['Katara', 'Avatar: The Legend of Aang'])
+table.add_row(['Luke Skywalker', 'Star Wars'])
+table.set_header(['Character', 'Storyline'])
+table.print(cell_padding=4, header_upper=True, row_border=True, auto_id=True, show_header=True)
+
+print('\n\nTable fed with a pre-created dataset of lists:\n')
+
+dataset_list = [
+    ['Hermione Granger', 'Harry Potter'],
+    ['Simon Spier', 'Simon vs. the Homo Sapiens Agenda'],
+    ['Ally', 'A Star is Born'],
+    ['Katara', 'Avatar: The Legend of Aang'],
+    ['Luke Skywalker', 'Star Wars']
+]
+
+table_list = TablePrinter()
+table_list.set_header(['Character', 'Storyline'])
+table_list.set_dataset(dataset_list)
+table_list.print(auto_id=True, cell_padding=4, show_header=True, row_border=True)
+
+print('\n\nTable fed with a pre-created dataset of dictionaries:\n')
+
+dataset_dict = [
+    {'id': 1, 'name': 'Hermione Granger', 'storyline': 'Harry Potter'},
+    {'id': 5, 'name': 'Simon Spier', 'storyline': 'Simon vs. the Homo Sapiens Agenda'},
+    {'id': 3, 'name': 'Ally', 'storyline': 'A Star is Born'},
+    {'id': 18, 'name': 'Katara', 'storyline': 'Avatar: The Legend of Aang'},
+    {'id': 11, 'name': 'Luke Skywalker', 'storyline': 'Star Wars'}
+]
+
+table_dict = TablePrinter()
+table_dict.set_header(header=['Character', 'Storyline'])
+table_dict.set_dataset(dataset_dict, ignore_keys=['id'])
+table_dict.print(auto_id=True, cell_padding=4, show_header=True, row_border=True)
---
+++ printer.py
@@ -0,0 +1,95 @@
+#=================== ABOUT ===================#
+# table_printer_py/printer.py
+# A python table printer for a given dataset
+# Author: Martín Figueroa Padilla
+# Version = 1.0.0
+#=============================================#
+
+class TablePrinter:
+    def __init__(self):
+        self.__dataset = []
+        self.__header = []
+
+    def add_row(self, row=[], times=1):
+        if isinstance(row, list):
+            for i in range(times):
+                self.__dataset.append(row)
+
+    def set_dataset(self, dataset, ignore_keys=[]):
+        if isinstance(dataset, list):
+            if isinstance(dataset[0], dict):
+                self.__dataset = []
+                for row in dataset:
+                    temp = []
+                    for key in row.keys():
+                        if key not in ignore_keys:
+                            temp.append(row[key])
+                    self.__dataset.append(temp)
+            else:
+                self.__dataset = dataset
+
+    def set_header(self, header):
+        if isinstance(header, list):
+            self.__header = header
+
+    def print(self, show_header=True, header_upper=True, cell_padding=0, cell_min_width=0, row_border=False, auto_id=False):
+        # Initial setup
+        if len(self.__header) == 0:
+            show_header = False
+        dataset = self.__dataset.copy()
+        header = self.__header.copy() if not header_upper else [text.upper() for text in self.__header]
+        header = header if not auto_id else ['ID'] + header
+
+        # Calculate table columns
+        cols = 0 if not show_header else len(header)
+        for row in dataset:
+            if len(row) > cols:
+                cols = len(row)
+        if auto_id and not show_header: cols += 1
+
+        # Standarize columns on every row and header
+        if show_header and len(header) < cols:
+            header = header + [''] * (cols - len(header))
+        row_id = 1
+        for i in range(0, len(dataset)):
+            if auto_id:
+                dataset[i] = [row_id] + dataset[i]
+                row_id += 1
+            if len(dataset[i]) < cols:
+                dataset[i] = dataset[i] + [''] * (cols - len(dataset[i]))
+
+        # Calculate columns widths
+        cols_width = [0] * cols if not show_header else [len(text) for text in header]
+        for row in dataset:
+            for i in range(0, cols):
+                if len(str(row[i])) > cols_width[i]:
+                    cols_width[i] = len(str(row[i]))
+                if cols_width[i] < cell_min_width:
+                    cols_width[i] = cell_min_width
+
+        # Print header of table
+        if show_header:
+            header_repr = ''
+            header_div = ''
+            for i in range(0, len(header)):
+                text = header[i]
+                space = cols_width[i] - len(text)
+                header_repr += '%s%s%s%s%s' \
+                    %(' ' * cell_padding, text, ' ' * space, ' ' * cell_padding, '|' if i < len(header) - 1 else '')
+                header_div += '%s%s' %('=' * (2 * cell_padding + space + len(text)), '|' if i < len(header) - 1 else '')
+            print(header_repr)
+            print(header_div)
+
+        # Print dataset of table
+        row_count = 0
+        for row in dataset:
+            row_repr = ''
+            row_div = ''
+            for i in range(0, len(row)):
+                space = cols_width[i] - len(str(row[i]))
+                row_repr += '%s%s%s%s%s' \
+                    %(' ' * cell_padding, row[i], ' ' * space, ' ' * cell_padding, '|' if i < len(row) - 1 else '')
+                if row_border:
+                    row_div += '%s%s' %('-' * (2 * cell_padding + len(str(row[i])) + space), '|' if i < len(row) - 1 else '')
+            print(row_repr + ('\n%s' %row_div if row_border and row_count < len(dataset) - 1 else ''))
+            row_count += 1
